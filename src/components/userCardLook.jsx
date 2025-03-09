1e415418c13066f2ad8031adf1ec8ac93aa64a9d
Gautam Dhodi <gautamdhodi02@gmail.com>
2025-03-09 11:06:30 +0530

    created a card how it looks after editing

---
+++ src/components/userCardLook.jsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { removeUserFromFeed } from "../utils/feedSlice";
+
+const UserCard = ({ user }) => {
+  const { _id, firstName, lastName, photoUrl, age, gender, about } = user;
+  const dispatch = useDispatch();
+
+  const handleSendRequest = async (status, userId) => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/request/send/" + status + "/" + userId,
+        {},
+        { withCredentials: true }
+        
+      );
+      dispatch(removeUserFromFeed(userId));
+    } catch (err) {
+        // res.send(err)
+    }
+  };
+
+  return (
+    
+    <div className="card bg-base-300 w-96 shadow-xl">
+      <figure>
+        <img src={user.photoUrl} alt="photo" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{firstName + " " + lastName}</h2>
+        {age && gender && <p>{age + ", " + gender}</p>}
+        <p>{about}</p>
+      </div>
+    </div>
+  );
+};
+export default UserCard;
