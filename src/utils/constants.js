fa5006a4a28caa7eab0fa4f08a86afba3b5fff82
Gautam Dhodi <gautamdhodi02@gmail.com>
2025-09-25 17:03:50 +0530

    fixed some bugs

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,11 @@
 export const BASE_URL = "https://connectify-backend-1-c1hv.onrender.com";
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://connectify-frontend-lime.vercel.app"
+];
+
+app.use(require("cors")({
+  origin: allowedOrigins,
+  credentials: true
+}));
